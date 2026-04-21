be1d11192ede206e9d9f225f0d42d28d9fb64c5c
QaisCyber <qcyber582@gmail.com>
2026-04-21 13:08:31 +0300

    Enhance startup script for Juice Shop
    Updated messages for clarity and added instructions.

--- start-juice.sh
+++ start-juice.sh
@@ -3,12 +3,14 @@
 echo "Stopping old container..."
 sudo docker rm -f juice-shop 2>/dev/null
 
-echo "Starting Juice Shop..."
+echo "Starting OWASP Juice Shop Lab..."
+echo "-----------------------------------"
+echo "Open Browser:"
+echo "http://localhost:3000"
+echo "-----------------------------------"
 
 sudo docker run 
 --name juice-shop 
 -p 3000:3000 
 --restart unless-stopped 
 bkimminich/juice-shop
-
-echo "Juice Shop Running..."
